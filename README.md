d49330d205889c92a8ac5ef2b01b850122db48b6
Victor Johnsson <victor.johnsson@gmail.com>
2021-12-30 12:34:54 +0100

    copy paste complete

---
+++ README.md
@@ -0,0 +1,8 @@
+Eclipse
+gradle support, found in eclipse download site
+testng support,
+Select Help / Install New Software...
+Enter the update site URL in "Work with:" field:
+Update site for release: https://testng.org/testng-eclipse-update-site.
+Make sure the check box next to URL is checked and click Next.
+Eclipse will then guide you through the process.
---
+++ app/src/test/java/guilded/rose/GuildedRoseTest.java
@@ -0,0 +1,17 @@
+package guilded.rose;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+
+class GildedRoseTest {
+
+    @Test
+    void foo() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("fixme", app.items[0].name);
+    }
+
+}
